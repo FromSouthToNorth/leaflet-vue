1294c2acdb758efe5c165de5249969e71b3992a7
huangyu <598050554@qq.com>
2023-12-01 18:04:36 +0800

    add

--- src/utils/geo/leaflet/layers/polyline.ts
+++ src/utils/geo/leaflet/layers/polyline.ts
@@ -1,4 +1,8 @@
-import L, { LatLngExpression, PolylineOptions } from 'leaflet';
+import { geoIdentity, geoStream } from 'd3-geo';
+import type { LineString } from 'geojson';
+import L, { LatLngExpression, Polyline, PolylineOptions } from 'leaflet';
+
+import { getClipExtent, projection } from '../util';
 
 export const polyline = (
   latlngs: LatLngExpression[] | LatLngExpression[][],
@@ -6,3 +10,25 @@ export const polyline = (
 ) => {
   return L.polyline(latlngs, options).bindPopup(options.name);
 };
+
+export interface Segments {
+  d: string;
+  id: string;
+  index: number;
+}
+
+export function markerSegments(dt: number) {
+  return function (layer: LineString | Polyline) {
+    const i = 0;
+    const offset = dt;
+    const segments = [];
+    const clip = geoIdentity().clipExtent(getClipExtent()).stream;
+    const geoJSON: LineString = layer instanceof Polyline ? layer.toGeoJSON() : layer;
+    const coordinates = [...geoJSON.coordinates];
+    let a: number[] | null, b: number[] | null;
+    geoStream({
+      type: 'LineString',
+      coordinates,
+    },projection.stream(clip({}));
+  };
+}
--- src/utils/geo/leaflet/util/index.ts
+++ src/utils/geo/leaflet/util/index.ts
@@ -1,12 +1,62 @@
-import L, { LatLngExpression, Point } from 'leaflet';
+import { geoTransform } from 'd3-geo';
+import type { GeometryObject } from 'geojson';
+import L, { LatLngBounds, LatLngExpression, Point } from 'leaflet';
 import { toRaw } from 'vue';
 
 import { map } from '@/hooks/web/map/useMap';
 
+const _map = toRaw(map.value);
+
 export function latlngToPoint(latlng: LatLngExpression): Point {
-  return toRaw(map.value).latLngToLayerPoint(latlng);
+  return _map.latLngToLayerPoint(latlng);
 }
 
 export const latlng = (lat: number, lng: number) => {
   return L.latLng(lat, lng);
 };
+
+export function latLngToLayerPoint(latlng: LatLngExpression) {
+  return _map.latLngToLayerPoint(latlng);
+}
+
+function projectPoint(lat: number, lng: number) {
+  const point = _map.latLngToLayerPoint(L.latLng(lat, lng));
+  this.stream.point(point.x, point.y);
+}
+
+export const projection = geoTransform({
+  point: projectPoint,
+});
+
+export function getBounds(): LatLngBounds {
+  return _map.getBounds();
+}
+
+export function getBbox() {
+  const { getNorthEast, getSouthWest } = getBounds();
+  const southWest = latLngToLayerPoint(getNorthEast());
+  const northEast = latLngToLayerPoint(getSouthWest());
+  const bbox = {
+    minX: southWest.x,
+    minY: northEast.y,
+    maxX: northEast.x,
+    maxY: southWest.y,
+  };
+  return bbox;
+}
+
+export function getClipExtent(): [[number, number], [number, number]] {
+  const _bbox = getBbox();
+  return [
+    [_bbox.minX, _bbox.minY],
+    [_bbox.maxX, _bbox.maxY],
+  ];
+}
+
+export function geometryToLayer(geoJSON: GeometryObject) {
+  return L.GeoJSON.geometryToLayer(geoJSON);
+}
+
+export function geoVecAngle(a: number[], b: number[]): number {
+  return Math.atan2(b[1] - a[1], b[0] - a[0]);
+}
